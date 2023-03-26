43ec3ff17f53e3c98d4fa5f0d1776ddf1e7662cb
zayez <zayezuno@protonmail.com>
2023-03-26 13:22:59 -0300

    add boot

--- hosts/volos/hardware-configuration.nix
+++ hosts/volos/hardware-configuration.nix
@@ -8,5 +8,14 @@
     "${modulesPath}/installer/scan/not-detected.nix"
   ];
 
+  # boot
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  # boot.loader.grub.useOSProber = useOSProber;
+  # boot.loader.grub.extraEntries = import ./multiboot.nix;
+  boot.kernelModules = [ "kvm-amd" ];
+  # boot.kernel.sysctl = { "net.ipv4.ip_forward" = 1; };
+
   fileSystems."/".device = "/dev/disk/by-label/nixos";
 }
