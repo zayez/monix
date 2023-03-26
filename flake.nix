e6cf40a7a8093960c3b1a908534005151737b408
zayez <zayezuno@protonmail.com>
2023-03-26 16:07:59 -0300

    update flake

--- flake.nix
+++ flake.nix
@@ -1,80 +1,38 @@
 {
-  description = "Unstable + HM";
+  description = "monix configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     home-manager = {
-      url = "github:rycee/home-manager/master";
-      inputs.nixpkgs.follows = "/nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
     };
   };
 
-  outputs = { self, home-manager, nixpkgs }:
-    let
-      pkgs = (import nixpkgs) {
-        system = "x86_64-linux";
+  outputs = { self, home-manager, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    user = "onion";
+    host = "londo";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [
+          home-manager.nixosModules.home-manager
+          (import (./hosts + "/${host}"))
+        ];
       };
-
-      hosts = map (pkgs.lib.removeSuffix ".nix") (
-        pkgs.lib.attrNames (
-          pkgs.lib.filterAttrs
-            (_: entryType: entryType == "regular")
-            (builtins.readDir ./hosts)
-        )
-      );
-
-      build-target = host: {
-        name = host;
-
-        value = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          modules = [
-            nixpkgs.nixosModules.notDetected
-            home-manager.nixosModules.home-manager
-            (import (./hosts + "/${host}"))
-          ];
-        };
-      };
-
-      live-usb = {
-        name = "live-usb";
-        value = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-base.nix"
-            nixpkgs.nixosModules.notDetected
-            home-manager.nixosModules.home-manager
-            (import ./live-usb/live-usb.nix)
-          ];
-        };
-      };
-
-      live-usb-min = {
-        name = "live-usb-min";
-        value = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-            nixpkgs.nixosModules.notDetected
-            home-manager.nixosModules.home-manager
-            (import ./live-usb/min.nix)
-          ];
-        };
-      };
-    in
-      {
-        nixosConfigurations = builtins.listToAttrs (
-          pkgs.lib.flatten (
-            map
-              (
-                host: [
-                  (build-target host)
-                ]
-              )
-              hosts ++ [ live-usb live-usb-min ]
-          )
-        );
-      };
+    };
+  };
 }
