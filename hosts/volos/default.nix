e63bdf591d82ce2abf4a5feaf7a5e6e906f4ed91
zayez <zayezuno@protonmail.com>
2023-03-30 19:48:25 -0300

    add ntfs support

--- hosts/volos/default.nix
+++ hosts/volos/default.nix
@@ -20,6 +20,8 @@ in
     (import ../../system/shared (args // { user = user; name = name; }))
   ];
 
+  boot.supportedFilesystems = [ "ntfs" ];
+
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   # boot.loader.grub.device = "/dev/sda";
